227c81dd75b53aa359ad161006074d52dce1bdeb
Barbosa <jvbarbosadeoliveira00@gmail.com>
2025-05-07 22:48:09 -0300

    Aba de navegação aprimorada.

--- ttiquete/app/tabs/_layout.tsx
+++ ttiquete/app/tabs/_layout.tsx
@@ -1,10 +1,37 @@
-import { Tabs } from "expo-router";
+import { Tabs } from 'expo-router';
 
-export default function TabLayout(){
-    return(
-        <Tabs>
-            <Tabs.Screen name="index" options={{title: 'Home'}} />
-            <Tabs.Screen name="cart" options={{title: 'Cart'}} />
-        </Tabs>
-    );
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#ffd33d',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: 'Cart',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'cart-sharp' : 'cart-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+  </Tabs>
+  );
 }
+   
+  
+
+  
