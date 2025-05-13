59666b7a2088f248444a61212b0484bace8b135b
Cainã Pessoa <cainapessoa@gmail.com>
2025-05-13 12:46:05 -0300

    remove slot

--- ttiquete/src/app/_layout.tsx
+++ ttiquete/src/app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "expo-router";
-import { Slot } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
 
 import { initializeDatabase } from "../database/initializeDatabase";
@@ -8,7 +7,6 @@ export default function RootLayout() {
   return (
     <SQLiteProvider databaseName="myDatabase.db" onInit={initializeDatabase}>
       <>
-        <Slot />
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
